53886c6b5983f07bd9c0e1b7eb67cb09f11a71c1
tigawanna <denniskinuthiaw@gmail.com>
2022-04-18 12:50:03 +0300

    rot-13-fns

--- printf_rot13/change_hex_low.c
+++ printf_rot13/change_hex_low.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- * change_base - changes number base
+ * change_hex_low - changes number base
  * @num: input number
  * @base: base to convert
  * Return: pointer to array - with the number converted
  */
-char *change_base(unsigned int num, int base)
+char *change_hex_low(unsigned int num, int base)
 {
-char symbols[] = "0123456789ABCDEF";
+char symbols[] = "0123456789abcdef";
 static char array[100];
 char *ptr;
 
